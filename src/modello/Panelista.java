9e44d9082ee0f247b96a6f21d120488641f2e028
khawlaOuaadou <khawla.ouaadou01@universitadipavia.it>
2025-02-12 20:49:31 +0100

    Inizio scrittura di SystemPubblicazionePanel

---
+++ src/modello/Panelista.java
@@ -0,0 +1,9 @@
+package modello;
+
+public class Panelista extends Utente {
+
+    public Panelista() {
+        //..
+    }
+
+}
---
+++ src/modello/creazionePanel/Sondaggio.java
@@ -0,0 +1,51 @@
+package modello.creazionePanel;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Sondaggio {
+    private ArrayList<Slot> slot;
+    private boolean approvato;
+    private LocalTime oraInizio; // queste due ore sono utili per capire se i
+    private LocalTime oraFine; // panelisti potranno acnora visualizzare il sondaggio o no,
+    // in caso contrario non sarà caricato dal database sui sondaggi visualizzabili dai panelisti.
+
+
+    public Sondaggio() {
+        this.slot = new ArrayList<>();
+        this.approvato = false;
+    }
+
+    public ArrayList<Slot> getSondaggio() {
+        return slot;
+    }
+     public void aggiungiSlot(Slot slot ){
+        this.slot.add(slot);
+     }
+
+     public ArrayList<Slot> getSondaggioSoldi() {
+        return slot;
+     }
+
+     public boolean getApprovato() {
+        return approvato;
+     }
+
+     public void setApprovato(boolean approvato) {
+        this.approvato = approvato;
+     }
+
+     public LocalTime getOraInizio() {
+        return oraInizio;
+     }
+     public void setOraInizio(LocalTime oraInizio) {
+        this.oraInizio = oraInizio;
+     }
+
+     public LocalTime getOraFine() {
+        return oraFine;
+     }
+     public void setOraFine(LocalTime oraFine) {
+        this.oraFine = oraFine;
+     }
+}
