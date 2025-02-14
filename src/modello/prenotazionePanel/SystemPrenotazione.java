771364af1045120f69de0e977f6773afe217088e
Tommaso Ghisolfi <Tommaso Ghisolfi@LAPTOP-0ME5AFU8.homenet.telecomitalia.it>
2025-02-14 14:31:04 +0100

    Creazione SystemPrenotazione

--- src/modello/prenotazionePanel/SystemPrenotazione.java
+++ src/modello/prenotazionePanel/SystemPrenotazione.java
@@ -1,6 +1,28 @@
 package modello.prenotazionePanel;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import modello.creazionePanel.Slot;
+
 public class SystemPrenotazione {
 
+	public SystemPrenotazione() {
+		
+		
+	}
+	
+	
+	public void prenotazione(Slot s) {
+		
+		LocalDate ld = s.getData();
+		LocalTime lt = s.getTime();
+		
+		//mi serve il nome del panelista che si è prenotato
+		//ma per far ciò ho bisogno di fare query al database
+		//oppure aggiungere un metodo in slot che permetta,
+		//per ora, di aggiungere panelisti all?attributo prenotati
+	}
+	
 	
 }
