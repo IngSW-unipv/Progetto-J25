af8f6ddf15099bab155d399ce0cb32c7a115f7c5
Arald <Arald@macs>
2025-02-16 16:19:03 +0100

    Aggiornamento prenotazioneInsaccatore

--- src/modello/Insaccatore.java
+++ src/modello/Insaccatore.java
@@ -1,6 +1,27 @@
 package modello;
 
 public class Insaccatore extends Panelista {
-	int orelimite;
+	private int orelimite;
+	private int limitecanc;
+	
+	public Insaccatore(String email, String nome, int orelimite) {
+		super(email, nome);
+		this.orelimite = orelimite; 
+		limitecanc=0;
+	}
+
+	public int getOrelimite() {
+		return orelimite;
+	}
+
+	public void setOrelimite(int orelimite) {
+		this.orelimite = orelimite;
+	}
+	
+	
+	
+	
+	//METODI UTILI: 
+	
 	
 }
---
+++ src/modello/prenotazioneInsaccatore/Giorno.java
@@ -0,0 +1,14 @@
+package modello.prenotazioneInsaccatore;
+import java.util.ArrayList;
+
+public class Giorno {
+	private eunum nome;
+	private ArrayList<Turno> turni;
+	
+	
+	public Giorno() {
+		
+	}
+	
+	
+}
