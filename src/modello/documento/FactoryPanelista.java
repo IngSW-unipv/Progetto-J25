209f2ff748a634145706f4861b705f51da30dd18
Tommaso Ghisolfi <Tommaso Ghisolfi@LAPTOP-0ME5AFU8.homenet.telecomitalia.it>
2025-02-18 19:34:28 +0100

    Creazione classi e test per DocumentoRiepilogo

---
+++ src/modello/documento/FactoryPanelista.java
@@ -0,0 +1,26 @@
+package modello.documento;
+
+import java.time.LocalDate;
+
+import modello.Insaccatore;
+
+import modello.Panelista;
+import modello.TipoUtente;
+
+public class FactoryPanelista {
+
+	public static Panelista CreaUtente(TipoUtente u, String email, String nome, String cognome, String luogoNascita, LocalDate dataNascita, String codiceFiscale, String residenza, double oreLavoro) {
+		
+		switch(u) {
+		
+			case PANELISTA:
+
+				return new Panelista(email, nome, cognome, luogoNascita, dataNascita, codiceFiscale, residenza, oreLavoro);
+
+			case INSACCATORE:
+				return new Insaccatore(email, nome, oreLavoro);
+			default: throw new IllegalArgumentException("Utente non riconosciuto");
+		}
+		
+	}
+}
