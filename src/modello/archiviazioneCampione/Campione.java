9f116bba3dfa5cafcb0a435e75136b0f96d65461
Tommaso Ghisolfi <Tommaso Ghisolfi@LAPTOP-0ME5AFU8.homenet.telecomitalia.it>
2025-02-20 18:32:55 +0100

    Creazione CampioneDao e testing

--- src/modello/archiviazioneCampione/Campione.java
+++ src/modello/archiviazioneCampione/Campione.java
@@ -1,11 +1,18 @@
 package modello.archiviazioneCampione;
 
+import java.time.LocalDate;
+
 public class Campione {
 
 	private int id;
+	private String stato;
+	private LocalDate dataArrivo;
+	
+	public Campione(int id, String stato, LocalDate dataArrivo) {
 
-	public Campione(int id) {
 		this.id = id;
+		this.stato = stato;
+		this.dataArrivo = dataArrivo;
 	}
 
 	public int getId() {
@@ -15,6 +22,23 @@ public class Campione {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getStato() {
+		return stato;
+	}
+
+	public void setStato(String stato) {
+		this.stato = stato;
+	}
+
+	public LocalDate getDataArrivo() {
+		return dataArrivo;
+	}
+
+	public void setDataArrivo(LocalDate dataArrivo) {
+		this.dataArrivo = dataArrivo;
+	}
+
 	
 	
 }
