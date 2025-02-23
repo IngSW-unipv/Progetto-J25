4c66538d635f646d410b54412d873048bda1e1bf
macsimenco <maxim.araldi01@universitadipavia.it>
2025-02-23 16:24:44 +0100

    .

--- src/jdbc/dao/max/ITurnoDAO.java
+++ src/jdbc/dao/max/ITurnoDAO.java
@@ -1,9 +1,10 @@
 
 package jdbc.dao.max;
+import java.util.ArrayList;
 import java.util.List;
 import modello.prenotazioneInsaccatore.*;
 
 public interface ITurnoDAO extends GenericoDAO<Turno>{
-		
+	 public ArrayList<Turno> recuperaTurniGiorno(Giorno giorno);
 }
 
