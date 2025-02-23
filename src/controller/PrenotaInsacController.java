71b2e1187c1614324d626a4a3ad45c0389f6e7bf
macsimenco <maxim.araldi01@universitadipavia.it>
2025-02-23 11:45:47 +0100

    aggiornamento

--- src/controller/PrenotaInsacController.java
+++ src/controller/PrenotaInsacController.java
@@ -3,15 +3,22 @@ import modello.prenotazioneInsaccatore.*;
 import view.prenotazioneInsaccatore.*;
 
 public class PrenotaInsacController {
-	private SystemPrenotaTurnoInsacc modello;
 	private ViewInsaccatore interf;
+	private TestFacadeSingletonMax facade;
 	
-	public PrenotaInsacController(SystemPrenotaTurnoInsacc modello, ViewInsaccatore interf) {
-		this.modello = modello;
+	//COSTRUTTORE:
+	public PrenotaInsacController( ViewInsaccatore interf) {
 		this.interf = interf;
 		//collego la view al controller:
 		this.interf.setController(this);
+		this.facade = TestFacadeSingletonMax.getIstanza();
 	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
