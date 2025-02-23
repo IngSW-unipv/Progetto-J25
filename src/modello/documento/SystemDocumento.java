73596f7eeeba154987415466d604e771e4a9db24
Tommaso Ghisolfi <Tommaso Ghisolfi@LAPTOP-0ME5AFU8.homenet.telecomitalia.it>
2025-02-23 10:52:23 +0100

    Modifiche

--- src/modello/documento/SystemDocumento.java
+++ src/modello/documento/SystemDocumento.java
@@ -1,32 +1,24 @@
 package modello.documento;
 
-import jdbc.bean.IUserDAO;
-import jdbc.bean.UserDAO;
-import jdbc.dao.documento.IOreLavoroDAO;
-import jdbc.dao.documento.OreLavoroDAO;
+import jdbc.FacedeSingletonDB;
 import modello.Panelista;
 
 public class SystemDocumento {
 	
-	IUserDAO userDAO;
-	IOreLavoroDAO oreLavoroDAO;
 	
-	public SystemDocumento(IUserDAO userDAO, IOreLavoroDAO oreLavoroDAO) {
-		
-		this.userDAO = userDAO;
-		this.oreLavoroDAO = oreLavoroDAO;
-		
+	
+	public SystemDocumento() {
 		
 	}
 	
 	public Panelista restituisciPanelista(int id) {
 		
-		return userDAO.selectPanelista(id);
+		return FacedeSingletonDB.getInstance().getUserDAO().selectPanelista(id);
 	}
 	
 	public double oreLavoroPanelista(int id, String mese) {
 		
-		return oreLavoroDAO.selectOreLavoro(id, mese);
+		return FacedeSingletonDB.getInstance().getOreLavoroDAO().selectOreLavoro(id, mese);
 	}
 	
 	
