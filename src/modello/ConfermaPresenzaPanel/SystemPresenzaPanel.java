e8a9c4b94bffd4122c3ba679726421d72c8a7c2c
LKDres9 <andresignacio.andradecedeno01@universitadipavia.it>
2025-02-26 17:47:29 +0100

    Aggiunta classe SystemPresenzaPanel

---
+++ src/modello/ConfermaPresenzaPanel/SystemPresenzaPanel.java
@@ -0,0 +1,17 @@
+package modello.ConfermaPresenzaPanel;
+/*
+ * Questa classe rappresenta il livello di business logic per gestire le operazioni
+ * relative ai panel, ovvero la conferma della presenza di un panelista.
+ * Questa classe è stata creata per implementare il caso d'uso 
+ * UC11: Conferma presenza panelista
+ * UC12: Visualizzazione Ore Lavorate
+ */
+
+
+
+
+
+ 
+public class SystemPresenzaPanel {
+    
+}
